17b6ac390ae4506471ea00b4a9e5972c18747d12
ksj <shinde13@naver.com>
2018-12-26 10:44:55 +0900

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { BrowserModule } from '@angular/platform-browser'; //1
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here //1
 
 import { AppComponent }  from './app.component'; //2
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({ //3
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { } //4
